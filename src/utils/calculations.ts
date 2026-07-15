2af0a23e0ec04b0e1fd259dac141c068d34441d0
Kowshika Dorraju <kowshikadorraju03@gmail.com>
2026-07-15 18:07:07 +0530

    Initial commit

---
+++ src/utils/calculations.ts
@@ -0,0 +1,37 @@
+export function calculateAfterHarvest(
+  capitalGains: any,
+  selectedHoldings: any[]
+) {
+  let stcgProfit = capitalGains.stcg.profits;
+  let stcgLoss = capitalGains.stcg.losses;
+
+  let ltcgProfit = capitalGains.ltcg.profits;
+  let ltcgLoss = capitalGains.ltcg.losses;
+
+  selectedHoldings.forEach((item) => {
+    // Short Term Gain/Loss
+    if (item.stcg.gain > 0) {
+      stcgProfit -= item.stcg.gain;
+    } else {
+      stcgLoss += Math.abs(item.stcg.gain);
+    }
+
+    // Long Term Gain/Loss
+    if (item.ltcg.gain > 0) {
+      ltcgProfit -= item.ltcg.gain;
+    } else {
+      ltcgLoss += Math.abs(item.ltcg.gain);
+    }
+  });
+
+  return {
+    stcg: {
+      profits: stcgProfit,
+      losses: stcgLoss,
+    },
+    ltcg: {
+      profits: ltcgProfit,
+      losses: ltcgLoss,
+    },
+  };
+}
